ad74755c142d514ae91c26d8db012030b50d2511
Java Thinked <59421318+JavaThinked@users.noreply.github.com>
2023-01-16 18:55:15 -0500

    Error on Dev while computing Final Result, set names to upper by default (#26)

--- src/main/java/com/javathinked/application/numerology/service/core/LoveCompatibility.java
+++ src/main/java/com/javathinked/application/numerology/service/core/LoveCompatibility.java
@@ -8,10 +8,10 @@ public class LoveCompatibility implements NumerologyComputer {
 
     @Override
     public int computeNumberFor(Person... person) {
-        var fullName = person[0].getFirstName() +
-                person[0].getLastName() +
-                person[1].getFirstName() +
-                person[1].getLastName();
+        var fullName = person[0].getFirstName().toUpperCase() +
+                person[0].getLastName().toUpperCase() +
+                person[1].getFirstName().toUpperCase() +
+                person[1].getLastName().toUpperCase();
         var characterToCompute = 0;
         for(int i = 0; i < fullName.length(); i++) {
             characterToCompute += NumerologyValue.getLetterValues().get(String.valueOf(fullName.charAt(i)));
